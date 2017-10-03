26018a8d23df444fae146bb774de6dda7f6b604d
jayzen <jayzen@foxmail.com>
2017-10-03 23:18:37 +0800

    add advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      login_in user
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
-      flash.new[:notice] = "email or password wrong"
+      flash.now[:notice] = "email or password wrong"
       render 'new'
     end
-    
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
