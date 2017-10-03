8be38759e8db2d60f6e1a0e1985540434230fc00
jayzen <jayzen@foxmail.com>
2017-10-03 20:41:27 +0800

    basic_login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      login_in user
+      redirect_to user
+    else
+      flash.new[:notice] = "email or password wrong"
+      render 'new'
+    end
+    
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root 'welcomes#index'
+
   get 'signup', to: "users#new"
   get 'static_pages/home'
   get 'static_pages/help'
-  
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
   resources :users
 end
